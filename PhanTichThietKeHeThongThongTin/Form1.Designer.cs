fd1ed41b35adf11c9b586af506976abd46b5aba1
Nguyen Cao Thiem <1851061743@e.tlu.edu.vn>
2020-12-31 23:30:27 +0700

    dasdas

--- PhanTichThietKeHeThongThongTin/Form1.Designer.cs
+++ PhanTichThietKeHeThongThongTin/Form1.Designer.cs
@@ -28,13 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dangnhap_email = new System.Windows.Forms.TextBox();
+            this.dangnhap_password = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(421, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hệ thống quản lý Ký túc xá Trường Đại học Thủy Lợi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Email người dùng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mật khẩu đăng nhập";
+            // 
+            // dangnhap_email
+            // 
+            this.dangnhap_email.Location = new System.Drawing.Point(194, 58);
+            this.dangnhap_email.Name = "dangnhap_email";
+            this.dangnhap_email.Size = new System.Drawing.Size(202, 20);
+            this.dangnhap_email.TabIndex = 3;
+            // 
+            // dangnhap_password
+            // 
+            this.dangnhap_password.Location = new System.Drawing.Point(194, 93);
+            this.dangnhap_password.Name = "dangnhap_password";
+            this.dangnhap_password.Size = new System.Drawing.Size(202, 20);
+            this.dangnhap_password.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(321, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Đăng nhập";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(473, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Vui lòng liên hệ với quản trị viên hệ thống nếu bạn muốn đăng ký tài khoản mới ho" +
+    "ặc đổi mật khẩu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(543, 185);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dangnhap_password);
+            this.Controls.Add(this.dangnhap_email);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập hệ thống";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox dangnhap_email;
+        private System.Windows.Forms.TextBox dangnhap_password;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
     }
 }
 
---
+++ PhanTichThietKeHeThongThongTin/Quanlysinhvien.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace PhanTichThietKeHeThongThongTin
+{
+    partial class Quanlysinhvien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.sinhvien_datagrid = new System.Windows.Forms.DataGridView();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.sinhvien_rad_nam = new System.Windows.Forms.RadioButton();
+            this.sinhvien_rad_nu = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sinhvien_btn_xoa = new System.Windows.Forms.Button();
+            this.sinhvien_btn_them = new System.Windows.Forms.Button();
+            this.sinhvien_txt_diachi = new System.Windows.Forms.TextBox();
+            this.sinhvien_txt_sodienthoai = new System.Windows.Forms.TextBox();
+            this.sinhvien_txt_hoten = new System.Windows.Forms.TextBox();
+            this.sinhvien_time_ngaysinh = new System.Windows.Forms.DateTimePicker();
+            this.sinhvien_txt_masinhvien = new System.Windows.Forms.TextBox();
+            this.sinhvien_btn_sua = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sinhvien_datagrid)).BeginInit();
+            this.panel8.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.sinhvien_btn_sua);
+            this.panel1.Controls.Add(this.panel8);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.sinhvien_btn_xoa);
+            this.panel1.Controls.Add(this.sinhvien_btn_them);
+            this.panel1.Controls.Add(this.sinhvien_txt_diachi);
+            this.panel1.Controls.Add(this.sinhvien_txt_sodienthoai);
+            this.panel1.Controls.Add(this.sinhvien_txt_hoten);
+            this.panel1.Controls.Add(this.sinhvien_time_ngaysinh);
+            this.panel1.Controls.Add(this.sinhvien_txt_masinhvien);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(788, 131);
+            this.panel1.TabIndex = 0;
+            // 
+            // sinhvien_datagrid
+            // 
+            this.sinhvien_datagrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.sinhvien_datagrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sinhvien_datagrid.Location = new System.Drawing.Point(0, 131);
+            this.sinhvien_datagrid.Name = "sinhvien_datagrid";
+            this.sinhvien_datagrid.Size = new System.Drawing.Size(788, 301);
+            this.sinhvien_datagrid.TabIndex = 1;
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.sinhvien_rad_nam);
+            this.panel8.Controls.Add(this.sinhvien_rad_nu);
+            this.panel8.Location = new System.Drawing.Point(314, 54);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(118, 35);
+            this.panel8.TabIndex = 39;
+            // 
+            // sinhvien_rad_nam
+            // 
+            this.sinhvien_rad_nam.AutoSize = true;
+            this.sinhvien_rad_nam.Checked = true;
+            this.sinhvien_rad_nam.Location = new System.Drawing.Point(3, 11);
+            this.sinhvien_rad_nam.Name = "sinhvien_rad_nam";
+            this.sinhvien_rad_nam.Size = new System.Drawing.Size(47, 17);
+            this.sinhvien_rad_nam.TabIndex = 19;
+            this.sinhvien_rad_nam.TabStop = true;
+            this.sinhvien_rad_nam.Text = "Nam";
+            this.sinhvien_rad_nam.UseVisualStyleBackColor = true;
+            // 
+            // sinhvien_rad_nu
+            // 
+            this.sinhvien_rad_nu.AutoSize = true;
+            this.sinhvien_rad_nu.Location = new System.Drawing.Point(55, 11);
+            this.sinhvien_rad_nu.Name = "sinhvien_rad_nu";
+            this.sinhvien_rad_nu.Size = new System.Drawing.Size(39, 17);
+            this.sinhvien_rad_nu.TabIndex = 20;
+            this.sinhvien_rad_nu.Text = "Nữ";
+            this.sinhvien_rad_nu.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(25, 54);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 13);
+            this.label8.TabIndex = 38;
+            this.label8.Text = "Ngày sinh";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(260, 67);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(47, 13);
+            this.label9.TabIndex = 37;
+            this.label9.Text = "Giới tính";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(478, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 36;
+            this.label4.Text = "Địa chỉ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(465, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 35;
+            this.label5.Text = "Điện thoại";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(269, 24);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 34;
+            this.label6.Text = "Họ tên";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Mã sinh viên";
+            // 
+            // sinhvien_btn_xoa
+            // 
+            this.sinhvien_btn_xoa.Location = new System.Drawing.Point(693, 54);
+            this.sinhvien_btn_xoa.Name = "sinhvien_btn_xoa";
+            this.sinhvien_btn_xoa.Size = new System.Drawing.Size(75, 23);
+            this.sinhvien_btn_xoa.TabIndex = 30;
+            this.sinhvien_btn_xoa.Text = "Xóa";
+            this.sinhvien_btn_xoa.UseVisualStyleBackColor = true;
+            // 
+            // sinhvien_btn_them
+            // 
+            this.sinhvien_btn_them.Location = new System.Drawing.Point(693, 16);
+            this.sinhvien_btn_them.Name = "sinhvien_btn_them";
+            this.sinhvien_btn_them.Size = new System.Drawing.Size(75, 23);
+            this.sinhvien_btn_them.TabIndex = 29;
+            this.sinhvien_btn_them.Text = "Thêm";
+            this.sinhvien_btn_them.UseVisualStyleBackColor = true;
+            // 
+            // sinhvien_txt_diachi
+            // 
+            this.sinhvien_txt_diachi.Location = new System.Drawing.Point(524, 49);
+            this.sinhvien_txt_diachi.Name = "sinhvien_txt_diachi";
+            this.sinhvien_txt_diachi.Size = new System.Drawing.Size(121, 20);
+            this.sinhvien_txt_diachi.TabIndex = 28;
+            // 
+            // sinhvien_txt_sodienthoai
+            // 
+            this.sinhvien_txt_sodienthoai.Location = new System.Drawing.Point(524, 23);
+            this.sinhvien_txt_sodienthoai.Name = "sinhvien_txt_sodienthoai";
+            this.sinhvien_txt_sodienthoai.Size = new System.Drawing.Size(121, 20);
+            this.sinhvien_txt_sodienthoai.TabIndex = 27;
+            // 
+            // sinhvien_txt_hoten
+            // 
+            this.sinhvien_txt_hoten.Location = new System.Drawing.Point(314, 23);
+            this.sinhvien_txt_hoten.Name = "sinhvien_txt_hoten";
+            this.sinhvien_txt_hoten.Size = new System.Drawing.Size(121, 20);
+            this.sinhvien_txt_hoten.TabIndex = 26;
+            // 
+            // sinhvien_time_ngaysinh
+            // 
+            this.sinhvien_time_ngaysinh.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.sinhvien_time_ngaysinh.Location = new System.Drawing.Point(87, 51);
+            this.sinhvien_time_ngaysinh.Name = "sinhvien_time_ngaysinh";
+            this.sinhvien_time_ngaysinh.Size = new System.Drawing.Size(100, 20);
+            this.sinhvien_time_ngaysinh.TabIndex = 25;
+            // 
+            // sinhvien_txt_masinhvien
+            // 
+            this.sinhvien_txt_masinhvien.Location = new System.Drawing.Point(87, 23);
+            this.sinhvien_txt_masinhvien.Name = "sinhvien_txt_masinhvien";
+            this.sinhvien_txt_masinhvien.Size = new System.Drawing.Size(119, 20);
+            this.sinhvien_txt_masinhvien.TabIndex = 22;
+            // 
+            // sinhvien_btn_sua
+            // 
+            this.sinhvien_btn_sua.Location = new System.Drawing.Point(693, 88);
+            this.sinhvien_btn_sua.Name = "sinhvien_btn_sua";
+            this.sinhvien_btn_sua.Size = new System.Drawing.Size(75, 23);
+            this.sinhvien_btn_sua.TabIndex = 40;
+            this.sinhvien_btn_sua.Text = "Sửa";
+            this.sinhvien_btn_sua.UseVisualStyleBackColor = true;
+            // 
+            // Quanlysinhvien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 432);
+            this.Controls.Add(this.sinhvien_datagrid);
+            this.Controls.Add(this.panel1);
+            this.Name = "Quanlysinhvien";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quản lý sinh viên";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sinhvien_datagrid)).EndInit();
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView sinhvien_datagrid;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.RadioButton sinhvien_rad_nam;
+        private System.Windows.Forms.RadioButton sinhvien_rad_nu;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button sinhvien_btn_xoa;
+        private System.Windows.Forms.Button sinhvien_btn_them;
+        private System.Windows.Forms.TextBox sinhvien_txt_diachi;
+        private System.Windows.Forms.TextBox sinhvien_txt_sodienthoai;
+        private System.Windows.Forms.TextBox sinhvien_txt_hoten;
+        private System.Windows.Forms.DateTimePicker sinhvien_time_ngaysinh;
+        private System.Windows.Forms.TextBox sinhvien_txt_masinhvien;
+        private System.Windows.Forms.Button sinhvien_btn_sua;
+    }
+}
