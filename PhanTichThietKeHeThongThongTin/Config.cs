0e306b7cf74bfa0da2846edeefb28d83c011bd85
Nguyen Cao Thiem <1851061743@e.tlu.edu.vn>
2021-01-01 00:15:48 +0700

    Add connection

---
+++ PhanTichThietKeHeThongThongTin/Config.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhanTichThietKeHeThongThongTin
+{
+    class DataConfig
+    {
+        public static string CHUOIKETNOI_Main = "Data Source=HOME-PC;Initial Catalog=QuanLyKyTucXaDaiHocThuyLoiPTTKHTTT;User ID=sa;Password=********;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static string CHUOIKETNOI_Config = "Data Source=HOME-PC;Initial Catalog=QuanLyKyTucXaDaiHocThuyLoiPTTKHTTT; Integated Security = True ";
+        public static SqlConnection Conn = null;
+        //Kết nối Cơ sở dữ liệu: 
+        public static SqlCommand cmd = null;
+        //private SqlDataReader data = null;
+        public static SqlDataAdapter adapter = null;
+        public static DataTable table = null;
+        public static string query = null;
+
+        public static void openConnection()
+        {
+            DataConfig.Conn = new SqlConnection(DataConfig.CHUOIKETNOI_Main);
+            DataConfig.Conn.Open();
+        }
+
+        public static void closeConnection()
+        {
+            if (DataConfig.Conn != null)
+            {
+                DataConfig.Conn.Close();
+            }
+        }
+    }
+}
--- PhanTichThietKeHeThongThongTin/Quanlynhanvien.Designer.cs
+++ PhanTichThietKeHeThongThongTin/Quanlynhanvien.Designer.cs
@@ -28,8 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.nhanvien_datagrid = new System.Windows.Forms.DataGridView();
             this.panel6 = new System.Windows.Forms.Panel();
+            this.nhanvien_btn_sua = new System.Windows.Forms.Button();
             this.panel9 = new System.Windows.Forms.Panel();
             this.nhanvien_rad_nam = new System.Windows.Forms.RadioButton();
             this.nhanvien_rad_nu = new System.Windows.Forms.RadioButton();
@@ -48,21 +52,71 @@
             this.nhanvien_txt_email = new System.Windows.Forms.TextBox();
             this.nhanvien_txt_tennhanvien = new System.Windows.Forms.TextBox();
             this.nhanvien_txt_manhanvien = new System.Windows.Forms.TextBox();
-            this.nhanvien_btn_sua = new System.Windows.Forms.Button();
+            this.thongke_datagrid = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.thongke_btn_thongkesoluonghopdong = new System.Windows.Forms.Button();
+            this.thongke_time_ngayketthuc = new System.Windows.Forms.DateTimePicker();
+            this.thongke_time_ngaybatdau = new System.Windows.Forms.DateTimePicker();
+            this.thongke_btn_thongketinhtrangphong = new System.Windows.Forms.Button();
+            this.thongke_btn_baocaodoanhthu = new System.Windows.Forms.Button();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nhanvien_datagrid)).BeginInit();
             this.panel6.SuspendLayout();
             this.panel9.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.thongke_datagrid)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.nhanvien_datagrid);
+            this.tabPage1.Controls.Add(this.panel6);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Quản lý nhân viên";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.thongke_datagrid);
+            this.tabPage2.Controls.Add(this.panel2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Báo cáo thống kê";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // nhanvien_datagrid
             // 
             this.nhanvien_datagrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.nhanvien_datagrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.nhanvien_datagrid.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.nhanvien_datagrid.Location = new System.Drawing.Point(0, 142);
+            this.nhanvien_datagrid.Location = new System.Drawing.Point(3, 145);
             this.nhanvien_datagrid.Name = "nhanvien_datagrid";
-            this.nhanvien_datagrid.Size = new System.Drawing.Size(800, 308);
-            this.nhanvien_datagrid.TabIndex = 4;
+            this.nhanvien_datagrid.Size = new System.Drawing.Size(786, 276);
+            this.nhanvien_datagrid.TabIndex = 6;
             // 
             // panel6
             // 
@@ -84,10 +138,20 @@
             this.panel6.Controls.Add(this.nhanvien_txt_tennhanvien);
             this.panel6.Controls.Add(this.nhanvien_txt_manhanvien);
             this.panel6.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel6.Location = new System.Drawing.Point(0, 0);
+            this.panel6.Location = new System.Drawing.Point(3, 3);
             this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(800, 142);
-            this.panel6.TabIndex = 3;
+            this.panel6.Size = new System.Drawing.Size(786, 142);
+            this.panel6.TabIndex = 5;
+            // 
+            // nhanvien_btn_sua
+            // 
+            this.nhanvien_btn_sua.Enabled = false;
+            this.nhanvien_btn_sua.Location = new System.Drawing.Point(701, 77);
+            this.nhanvien_btn_sua.Name = "nhanvien_btn_sua";
+            this.nhanvien_btn_sua.Size = new System.Drawing.Size(75, 23);
+            this.nhanvien_btn_sua.TabIndex = 64;
+            this.nhanvien_btn_sua.Text = "Sửa";
+            this.nhanvien_btn_sua.UseVisualStyleBackColor = true;
             // 
             // panel9
             // 
@@ -244,39 +308,145 @@
             this.nhanvien_txt_manhanvien.Size = new System.Drawing.Size(119, 20);
             this.nhanvien_txt_manhanvien.TabIndex = 46;
             // 
-            // nhanvien_btn_sua
+            // thongke_datagrid
             // 
-            this.nhanvien_btn_sua.Enabled = false;
-            this.nhanvien_btn_sua.Location = new System.Drawing.Point(701, 77);
-            this.nhanvien_btn_sua.Name = "nhanvien_btn_sua";
-            this.nhanvien_btn_sua.Size = new System.Drawing.Size(75, 23);
-            this.nhanvien_btn_sua.TabIndex = 64;
-            this.nhanvien_btn_sua.Text = "Sửa";
-            this.nhanvien_btn_sua.UseVisualStyleBackColor = true;
+            this.thongke_datagrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.thongke_datagrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.thongke_datagrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.thongke_datagrid.Location = new System.Drawing.Point(3, 145);
+            this.thongke_datagrid.Name = "thongke_datagrid";
+            this.thongke_datagrid.Size = new System.Drawing.Size(786, 276);
+            this.thongke_datagrid.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.thongke_btn_thongkesoluonghopdong);
+            this.panel2.Controls.Add(this.thongke_time_ngayketthuc);
+            this.panel2.Controls.Add(this.thongke_time_ngaybatdau);
+            this.panel2.Controls.Add(this.thongke_btn_thongketinhtrangphong);
+            this.panel2.Controls.Add(this.thongke_btn_baocaodoanhthu);
+            this.panel2.Controls.Add(this.label21);
+            this.panel2.Controls.Add(this.label22);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(786, 142);
+            this.panel2.TabIndex = 5;
+            // 
+            // thongke_btn_thongkesoluonghopdong
+            // 
+            this.thongke_btn_thongkesoluonghopdong.Location = new System.Drawing.Point(372, 59);
+            this.thongke_btn_thongkesoluonghopdong.Name = "thongke_btn_thongkesoluonghopdong";
+            this.thongke_btn_thongkesoluonghopdong.Size = new System.Drawing.Size(260, 23);
+            this.thongke_btn_thongkesoluonghopdong.TabIndex = 64;
+            this.thongke_btn_thongkesoluonghopdong.Text = "Thống kê số lượng hợp đồng";
+            this.thongke_btn_thongkesoluonghopdong.UseVisualStyleBackColor = true;
+            // 
+            // thongke_time_ngayketthuc
+            // 
+            this.thongke_time_ngayketthuc.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.thongke_time_ngayketthuc.Location = new System.Drawing.Point(115, 88);
+            this.thongke_time_ngayketthuc.Name = "thongke_time_ngayketthuc";
+            this.thongke_time_ngayketthuc.Size = new System.Drawing.Size(99, 20);
+            this.thongke_time_ngayketthuc.TabIndex = 47;
+            // 
+            // thongke_time_ngaybatdau
+            // 
+            this.thongke_time_ngaybatdau.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.thongke_time_ngaybatdau.Location = new System.Drawing.Point(115, 58);
+            this.thongke_time_ngaybatdau.Name = "thongke_time_ngaybatdau";
+            this.thongke_time_ngaybatdau.Size = new System.Drawing.Size(99, 20);
+            this.thongke_time_ngaybatdau.TabIndex = 46;
+            // 
+            // thongke_btn_thongketinhtrangphong
+            // 
+            this.thongke_btn_thongketinhtrangphong.Location = new System.Drawing.Point(372, 26);
+            this.thongke_btn_thongketinhtrangphong.Name = "thongke_btn_thongketinhtrangphong";
+            this.thongke_btn_thongketinhtrangphong.Size = new System.Drawing.Size(260, 23);
+            this.thongke_btn_thongketinhtrangphong.TabIndex = 45;
+            this.thongke_btn_thongketinhtrangphong.Text = "Thống kê tình trạng phòng";
+            this.thongke_btn_thongketinhtrangphong.UseVisualStyleBackColor = true;
+            // 
+            // thongke_btn_baocaodoanhthu
+            // 
+            this.thongke_btn_baocaodoanhthu.Location = new System.Drawing.Point(372, 89);
+            this.thongke_btn_baocaodoanhthu.Name = "thongke_btn_baocaodoanhthu";
+            this.thongke_btn_baocaodoanhthu.Size = new System.Drawing.Size(260, 23);
+            this.thongke_btn_baocaodoanhthu.TabIndex = 43;
+            this.thongke_btn_baocaodoanhthu.Text = "Báo cáo doanh thu";
+            this.thongke_btn_baocaodoanhthu.UseVisualStyleBackColor = true;
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(24, 88);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(74, 13);
+            this.label21.TabIndex = 39;
+            this.label21.Text = "Ngày kết thúc";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Location = new System.Drawing.Point(22, 58);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(72, 13);
+            this.label22.TabIndex = 38;
+            this.label22.Text = "Ngày bắt đầu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(46, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 20);
+            this.label1.TabIndex = 65;
+            this.label1.Text = "Báo cáo thống kê";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 13);
+            this.label2.TabIndex = 66;
+            this.label2.Text = "Thống kê trên được dựa theo ngày lập";
             // 
             // Quanlynhanvien
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.nhanvien_datagrid);
-            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Quanlynhanvien";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Quản lý nhân viên";
+            this.Text = "Cán bộ quản lý";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.nhanvien_datagrid)).EndInit();
             this.panel6.ResumeLayout(false);
             this.panel6.PerformLayout();
             this.panel9.ResumeLayout(false);
             this.panel9.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.thongke_datagrid)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.DataGridView nhanvien_datagrid;
         private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button nhanvien_btn_sua;
         private System.Windows.Forms.Panel panel9;
         private System.Windows.Forms.RadioButton nhanvien_rad_nam;
         private System.Windows.Forms.RadioButton nhanvien_rad_nu;
@@ -295,6 +465,17 @@
         private System.Windows.Forms.TextBox nhanvien_txt_email;
         private System.Windows.Forms.TextBox nhanvien_txt_tennhanvien;
         private System.Windows.Forms.TextBox nhanvien_txt_manhanvien;
-        private System.Windows.Forms.Button nhanvien_btn_sua;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView thongke_datagrid;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button thongke_btn_thongkesoluonghopdong;
+        private System.Windows.Forms.DateTimePicker thongke_time_ngayketthuc;
+        private System.Windows.Forms.DateTimePicker thongke_time_ngaybatdau;
+        private System.Windows.Forms.Button thongke_btn_thongketinhtrangphong;
+        private System.Windows.Forms.Button thongke_btn_baocaodoanhthu;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label label2;
     }
 }
